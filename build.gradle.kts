acb05d70f2f54731df6cbf301fdbce449c22f7bf
mgroth0 <mgroth49@gmail.com>
2026-02-04 18:28:48 -0500

    update to correctly reproduce error

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,48 +9,57 @@ plugins {
 }
 
 allprojects {
-    plugins.apply("org.jetbrains.kotlin.multiplatform")
-    plugins.apply("com.android.kotlin.multiplatform.library")
-    repositories {
-        mavenCentral()
-        google()
-    }
 
-    plugins.withId("org.jetbrains.kotlin.multiplatform") {
-        plugins.withId("com.android.kotlin.multiplatform.library") {
-            kotlin {
-                applyHierarchyTemplate {
-                    common {
-                        group("commonJvmAndroid") {
-                            withJvm()
-                            withCompilations {
-                                /*
-                                Used to check if instance is `KotlinAndroidTarget` back when we used `com.android.library`, but it no longer works with `com.android.kotlin.multiplatform.library`:
-                                    `it is KotlinAndroidTarget`
+    if (name!="android") {
+        plugins.apply("org.jetbrains.kotlin.multiplatform")
+        plugins.apply("com.android.kotlin.multiplatform.library")
+        repositories {
+            mavenCentral()
+            google()
+        }
 
-                                    the replacement below is a best guess at what to do with the new plugin
+        plugins.withId("org.jetbrains.kotlin.multiplatform") {
+            plugins.withId("com.android.kotlin.multiplatform.library") {
+                kotlin {
+                    applyHierarchyTemplate {
+                        common {
+                            group("commonJvmAndroid") {
+                                withJvm()
+                                withCompilations {
+                                    /*
+                                    Used to check if instance is `KotlinAndroidTarget` back when we used `com.android.library`, but it no longer works with `com.android.kotlin.multiplatform.library`:
+                                        `it is KotlinAndroidTarget`
 
-                                 */
-                                it.platformType == KotlinPlatformType.androidJvm
-                                    || it.target.targetName == "android"
+                                        the replacement below is a best guess at what to do with the new plugin
+
+                                     */
+                                    it.platformType == KotlinPlatformType.androidJvm
+                                            || it.target.targetName == "android"
+                                }
                             }
                         }
                     }
-                }
-                jvm()
-                androidLibrary {
-                    namespace = project.name
-                    compileSdk = 36
+                    jvm()
+                    androidLibrary {
+                        namespace = project.name
+                        compileSdk = 36
+                    }
                 }
             }
         }
     }
+
+
 }
 
-kotlin {
-    sourceSets.getByName("commonJvmAndroidMain") {
-        dependencies {
-            implementation(project(":sub"))
+if (name!="android") {
+
+    kotlin {
+        sourceSets.getByName("commonJvmAndroidMain") {
+            dependencies {
+                implementation(project(":sub"))
+            }
         }
     }
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,12 @@ pluginManagement {
     }
     plugins {
         kotlin("multiplatform")  version "2.3.0"
-        id("com.android.kotlin.multiplatform.library") version "8.12.2"
+//        id("com.android.kotlin.multiplatform.library") version "8.13.2"
+//        id("com.android.kotlin.multiplatform.library") version "9.0.0-alpha06"
+        id("com.android.kotlin.multiplatform.library") version "9.0.0"
+        id("com.android.library") version "9.0.0"
     }
 }
 
 include(":sub")
+include(":sub:android")
